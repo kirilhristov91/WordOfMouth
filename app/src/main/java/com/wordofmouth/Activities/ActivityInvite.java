a38f3b527d264f23d2f0185d74efe25255892372
Kiril Hristov <kirilhristov91@gmail.com>
2016-02-12 18:21:13 +0000

    set alert dialog in Invite to ask the user if he/she is ok with inviting the clicked user

--- app/src/main/java/com/wordofmouth/Activities/ActivityInvite.java
+++ app/src/main/java/com/wordofmouth/Activities/ActivityInvite.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.net.ConnectivityManager;
@@ -38,20 +39,20 @@ public class ActivityInvite extends BaseActivity implements View.OnClickListener
     String[] usernames;
     private UserLocalStore userLocalStore;
     int tabToreturn;
+    int userClicked;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.activity_activity_invite);
-        /*Toolbar toolbar = (Toolbar) findViewById(R.id.tool_bar);
-        setSupportActionBar(toolbar);*/
-        userLocalStore = UserLocalStore.getInstance(this);
+
         Intent intent = getIntent();
         selectedListId = intent.getIntExtra("listId", 0);
         listName = intent.getStringExtra("name");
         tabToreturn = intent.getIntExtra("tab", 0);
         getSupportActionBar().setTitle("Invite to: " + listName);
+        userLocalStore = UserLocalStore.getInstance(this);
 
         searchView = (SearchView) findViewById(R.id.searchUsersField);
         searchView.setOnClickListener(new View.OnClickListener() {
@@ -68,7 +69,6 @@ public class ActivityInvite extends BaseActivity implements View.OnClickListener
 
     }
 
-    // TODO set an alertDialog to ask if the user is sure he wants to invite the selected user
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
@@ -127,41 +127,56 @@ public class ActivityInvite extends BaseActivity implements View.OnClickListener
                 new AdapterView.OnItemClickListener() {
                     @Override
                     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                        // list id, sharedWithId
+                        userClicked = position;
+                        AlertDialog.Builder allertBuilder = new AlertDialog.Builder(ActivityInvite.this);
+                        allertBuilder.setMessage("Are you sure you want to share the list with that user?");
+                        allertBuilder.setCancelable(false);
 
-                        if (!isNetworkAvailable()) {
-                            showError("Network error! Check your internet connection and try again!");
-                        } else {
-                            int sharedWithId = users.get(position).getId();
-                            int currentUserId = userLocalStore.getUserLoggedIn().getId();
+                        allertBuilder.setPositiveButton("Invite", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                if (!isNetworkAvailable()) {
+                                    showError("Network error! Check your internet connection and try again!");
+                                } else {
+                                    int sharedWithId = users.get(userClicked).getId();
+                                    int currentUserId = userLocalStore.getUserLoggedIn().getId();
 
-                            final ProgressDialog progressDialog = new ProgressDialog(ActivityInvite.this,R.style.MyTheme);
-                            progressDialog.setCancelable(false);
-                            progressDialog.setProgressStyle(android.R.style.Widget_ProgressBar_Large);
-                            progressDialog.show();
+                                    final ProgressDialog progressDialog = new ProgressDialog(ActivityInvite.this,R.style.MyTheme);
+                                    progressDialog.setCancelable(false);
+                                    progressDialog.setProgressStyle(android.R.style.Widget_ProgressBar_Large);
+                                    progressDialog.show();
 
-                            ServerRequests serverRequests = ServerRequests.getInstance(ActivityInvite.this);
-                            serverRequests.inviteInBackground(selectedListId, currentUserId, sharedWithId, new SendInviteResponse() {
-                                @Override
-                                public void done(String response) {
-                                    progressDialog.dismiss();
-                                    if (response.equals("That person has already been invited to that list!\n")) {
-                                        showError("That person has already been invited to list '" + listName +"'!");
-                                    } else if (response.equals("Cannot invite the creator of the list!\n")) {
-                                        showError("That is the creator of the list '" + listName + "'! He/She already has acces to this list!");
-                                    } else if (response.equals("Timeout")) {
-                                        showError("Network error! Check your internet connection and try again!");
-                                    } else {
-                                        Intent myIntent = new Intent(ActivityInvite.this, ActivityItemsOfAList.class);
-                                        myIntent.putExtra("listId", selectedListId);
-                                        myIntent.putExtra("name", listName);
-                                        myIntent.putExtra("tab", tabToreturn);
-                                        startActivity(myIntent);
-                                        finish();
-                                    }
+                                    ServerRequests serverRequests = ServerRequests.getInstance(ActivityInvite.this);
+                                    serverRequests.inviteInBackground(selectedListId, currentUserId, sharedWithId, new SendInviteResponse() {
+                                        @Override
+                                        public void done(String response) {
+                                            progressDialog.dismiss();
+                                            if (response.equals("That person has already been invited to that list!\n")) {
+                                                showError("That person has already been invited to list '" + listName +"'!");
+                                            } else if (response.equals("Cannot invite the creator of the list!\n")) {
+                                                showError("That is the creator of the list '" + listName + "'! He/She already has acces to this list!");
+                                            } else if (response.equals("Timeout")) {
+                                                showError("Network error! Check your internet connection and try again!");
+                                            } else {
+                                                Intent myIntent = new Intent(ActivityInvite.this, ActivityItemsOfAList.class);
+                                                myIntent.putExtra("listId", selectedListId);
+                                                myIntent.putExtra("name", listName);
+                                                myIntent.putExtra("tab", tabToreturn);
+                                                startActivity(myIntent);
+                                                finish();
+                                            }
+                                        }
+                                    });
                                 }
-                            });
-                        }
+                            }
+                        });
+
+                        allertBuilder.setNegativeButton("Decline", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                            }
+                        });
+                        allertBuilder.create().show();
                     }
                 }
         );

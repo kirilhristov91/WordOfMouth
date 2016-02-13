5d454cd6625d43b46cda3d37c014b31f5002aa93
Kiril Hristov <kirilhristov91@gmail.com>
2016-02-13 23:36:07 +0000

    about is done

--- app/src/main/java/com/wordofmouth/Activities/ActivityAbout.java
+++ app/src/main/java/com/wordofmouth/Activities/ActivityAbout.java
@@ -1,11 +1,6 @@
 package com.wordofmouth.Activities;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.widget.TextView;
 
 import com.wordofmouth.R;
@@ -20,10 +15,12 @@ public class ActivityAbout extends BaseActivity {
         setContentView(R.layout.activity_activity_about);
 
         aboutText = (TextView) findViewById(R.id.aboutText);
-        String about = "Word Of Mouth is a mobile application that allows users ";
-
-
-
+        String about = "Word Of Mouth is a mobile application that allows users to share their interests with each other."
+                + " Here users manage 'shared' lists of interests with people they know and trust."
+                + " Users can create a list with a specific topic like 'Favourite movies'), add items to it and invite people "
+                + "- who they know would like wathcing movies - to manage the list. Each joined user will then be able to add new"
+                + " items to the list, rate the existing items and invite more people to join. It is easy, so try it and enjoy!";
+        aboutText.setText(about);
     }
 
     @Override
